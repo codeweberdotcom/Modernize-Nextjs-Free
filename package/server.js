619e897e92690b615d16896bd192c981e8f63185
Denis Korablev <info@codeweber.com>
2025-10-18 21:55:24 +0300

    33

--- package/server.js
+++ package/server.js
@@ -10,7 +10,7 @@ app.prepare().then(() => {
     handle(req, res);
   });
 
-  const PORT = process.env.PORT || 3000;
+  const PORT = process.env.PORT || 3002;
   server.listen(PORT, (err) => {
     if (err) throw err;
     console.log(`> Ready on http://localhost:${PORT}`);
